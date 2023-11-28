31a6b2817b54580f05ee97d75a87874dc4a3036a
Juan Pinardi <127795104+juanpinardi@users.noreply.github.com>
2023-11-28 17:45:38 -0300

    primer commit

---
+++ env.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+	define("URL_WEB", "https://mattprofe.com.ar/alumno/3853/app-estacion");
+	define("APP_NAME", "app-estacion");
+
+	define("DEBUG", false);
+	define("RELEASE", true);
+
+	define("CACHE", DEBUG);
+
+	// define("DB_HOST", "localhost");
+	// define("DB_USER", "morphyx");
+	// define("DB_PASS", "morphyx1234");
+	// define("DB_NAME", "morphyx");
+	// define("DB_PORT", 3306);
+
+
+ ?>
